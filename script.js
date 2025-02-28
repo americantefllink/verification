6ecd08bbfc3dc6daef1bf6810df8846b756c1dc1
americantefllink <serjxiehai@icloud.com>
2025-02-28 18:52:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkCertificate() {
     var certificateNumber = document.getElementById("certificateInput").value;
-    if (certificateNumber === "M102009195") {
+    if (certificateNumber === "M192029195") {
         window.location.href = "certificate.html";
     } else {
         alert("This certificate number is not valid");
